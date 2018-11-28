27859fcab17cc42371627d8fe491c22337a8b8ba
vladkorkach <vladkorkach.dev@gmail.com>
2018-11-28 15:42:23 +0200

    Removed unused

--- call_stats/static/call_stats/app.js
+++ call_stats/static/call_stats/app.js
@@ -1,34 +1,11 @@
 var chartObj = document.getElementById("src_data").innerText;
-console.log(chartObj)
-var chart1 = AmCharts.makeChart("chartdiv",
-  JSON.parse(chartObj)
-  // type: "serial",
-  // theme: "light",
-  // dataProvider: [{'date': '2018-11-27 18-37-15', 'Yahoo': 18, 'Google': 18}, {'date': '2018-11-27 18-37-45', 'Yahoo': 18, 'Google': 18}, {'date': '2018-11-27 18-38-15', 'Yahoo': 18, 'Google': 18}, {'date': '2018-11-27 18-38-45', 'Google': 18, 'Yahoo': 18}, {'date': '2018-11-27 18-39-15', 'Yahoo': 18, 'Google': 18}, {'date': '2018-11-27 18-39-45', 'Google': 18, 'Yahoo': 18}, {'date': '2018-11-27 18-40-15', 'Yahoo': 18, 'Google': 9}, {'date': '2018-11-27 18-40-16', 'Google': 9}, {'date': '2018-11-27 18-40-45', 'Google': 9, 'Yahoo': 9}, {'date': '2018-11-27 18-40-46', 'Google': 9, 'Yahoo': 9}, {'date': '2018-11-27 18-41-15', 'Google': 9, 'Yahoo': 9}],
-  // graphs: [
-  //   {
-  //     "bullet": "round",
-  //     valueField: "Yahoo",
-  //       "labelText": "[[key]]",
-  //       title:"yahoo"
-  //   },
-  //     {
-  //     bullet: "round",
-  //     valueField: "Google",
-  //         "labelText": "[[key]]",
-  //         title:"google"
-  //   }
-  // ],
-  //   "legend": {
-  //       "useGraphSettings": true,
-  // },
-  // categoryField: "date",
-  // dataDateFormat: "YYYY-MM-DD JJ:NN:SS",
-  // categoryAxis: {
-  //   parseDates: true,
-  //   minPeriod: "ss"
-  // }
-);
+chartObj = JSON.parse(chartObj)
+console.log()
+if(chartObj.dataProvider.length === 0) {
+  document.getElementById('chartdiv').innerHTML = "No data to show";
+} else {
+  var chart1 = AmCharts.makeChart("chartdiv", chartObj);
+}
 
 // chart1.startEffect = "easeInSine";
 // chart1.startDuration = "0.7";
--- call_stats/views.py
+++ call_stats/views.py
@@ -1,31 +1,28 @@
-from django.shortcuts import render
 from django.template import loader
 from .models import CallStat
 from django.http import HttpResponse
 from django.db.models import Count, Sum
 import json
-from operator import itemgetter
 
 
 def generate_chart_object(names, data):
+    graphs = []
+
+    for n in names:
+        tmp = {
+            "bullet": "round",
+            "valueField": n,
+            "labelText": "[[key]]",
+            "title": n
+        }
+
+        graphs.append(tmp)
+
     chart_settings = {
         "type": "serial",
         "theme": "light",
         "dataProvider": data,
-        "graphs": [
-            {
-                "bullet": "round",
-                "valueField": "Yahoo",
-                "labelText": "[[key]]",
-                "title": "yahoo"
-            },
-            {
-                "bullet": "round",
-                "valueField": "Google",
-                "labelText": "[[key]]",
-                "title": "google"
-            }
-        ],
+        "graphs": graphs,
         "legend": {
             "useGraphSettings": True,
         },
@@ -44,18 +41,16 @@ def index(request):
     template = loader.get_template("call_stats/index.html")
     a = CallStat.objects.all().values('phone_dialed__callstat__date', 'phone_dialed__organization').annotate(total=Count('phone_dialed')).prefetch_related("phone_dialed")
     l = []
+    names = []
     tmp = {}
     for data in a:
-
+        names.append(data["phone_dialed__organization"])
         if "date" not in tmp:
             tmp["date"] = data["phone_dialed__callstat__date"].strftime('%Y-%m-%d %H-%M-%S')
-            print(tmp["date"])
             tmp[data["phone_dialed__organization"]] = data['total']
         else:
-            print("2", tmp["date"])
             if tmp["date"] == data["phone_dialed__callstat__date"].strftime('%Y-%m-%d %H-%M-%S'):
                 if data["phone_dialed__organization"] in tmp:
-                    print(tmp)
                     tmp[data["phone_dialed__organization"]] += data['total']
                 else:
                     tmp[data["phone_dialed__organization"]] = data['total']
@@ -64,13 +59,10 @@ def index(request):
                 tmp = {}
                 tmp["date"] = data["phone_dialed__callstat__date"].strftime('%Y-%m-%d %H-%M-%S')
                 tmp[data["phone_dialed__organization"]] = data['total']
-        # tmp = {
-        #     "date": data["phone_dialed__callstat__date"].strftime('%H-%M-%S'),
-        #     data["phone_dialed__organization"]: data['total']
-        # }
-        # l.append(tmp)
-    print(l)
-    chart_object = generate_chart_object(["Yahoo", "Google"], l)
+    # print(l)
+    myset = set(names)
+    names = list(myset)
+    chart_object = generate_chart_object(names, l)
     context = {
         "chart": json.dumps(chart_object)
     }
